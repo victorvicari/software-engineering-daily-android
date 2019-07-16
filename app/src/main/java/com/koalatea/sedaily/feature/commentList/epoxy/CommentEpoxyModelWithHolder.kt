d8747de9a71ea2e14c00891a5d517304dc2ade44
Victor <victor.a.vicari@gmail.com>
2019-07-16 17:39:11 -0300

    updated the epoxy view holder to bind the fields

--- app/src/main/java/com/koalatea/sedaily/feature/commentList/epoxy/CommentEpoxyModelWithHolder.kt
+++ app/src/main/java/com/koalatea/sedaily/feature/commentList/epoxy/CommentEpoxyModelWithHolder.kt
@@ -3,29 +3,40 @@ package com.koalatea.sedaily.feature.commentList.epoxy
 import android.widget.Button
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
+import com.google.android.material.button.MaterialButton
 import com.koalatea.sedaily.R
 
 @EpoxyModelClass(layout = R.layout.view_holder_comment)
 abstract class CommentEpoxyModelWithHolder : BaseCommentEpoxyModelWithHolder<CommentHolder>() {
 
     @EpoxyAttribute lateinit var replyClickListener: () -> Unit
+
+    @EpoxyAttribute
+    var upvoted: Boolean? = null
+    @EpoxyAttribute
+    var score: Int? = null
     @EpoxyAttribute
     lateinit var upvoteClickListener: () -> Unit
 
     override fun bind(holder: CommentHolder) {
         super.bind(holder)
 
-        holder.likesButton.setOnClickListener {
-            it.isEnabled = false
-            upvoteClickListener()
-        }
-
         holder.replayButton.setOnClickListener { replyClickListener() }
+
+        holder.likesButton.setIconResource(if (upvoted == true) R.drawable.vd_favorite else R.drawable.vd_favorite_border)
+        holder.likesButton.text = score?.let {
+            if (it > 0) {
+                it.toString()
+            } else {
+                ""
+            }
+        }
+        holder.likesButton.setOnClickListener { upvoteClickListener() }
     }
 
 }
 
 class CommentHolder : BaseCommentHolder() {
-    val likesButton by bind<Button>(R.id.likesButton)
     val replayButton by bind<Button>(R.id.replyButton)
+    val likesButton by bind<MaterialButton>(R.id.likesButton)
 }
